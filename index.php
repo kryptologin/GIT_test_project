5914a6ce6123699df9e82e7ef78b835135c0f228
YoctoBit <42770197+YoctoBit@users.noreply.github.com>
2019-12-25 16:52:42 +0300

    Добавил новую страницу newList

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ print ('
 </head>
 <body>
 <H1>ПРИВЕТ ДРУЗЬЯ</H1>
-<p>Я первый и только первый пункт</p>
+<p>Я <span>первый</span> и только первый пункт</p>
 
 </body>
 </html>
@@ -23,15 +23,27 @@ print ('<ol>
   <li>Я не я, если я не второй пункт</li>
   <li>Третий после стольких лет? Всегда!</li>
 </ol>');
-print ('новая строка в которой будут изминения для конфликта в пулреквест на GitHub<br>');
-print ('еще одна строка для обновления файла в GIT<br>');
+print ('&ltНовая строка в которой будут изминения для конфликта в пулреквест на GitHub&gt<br>');
+print ('Он взглянул в окно и подумал — <i>такого просто не может быть</i>!<br>');
 print ('новая строка в которой будут изминения для Enother_branch ветки<br>');
 
-print ('новая строка в которой будут изминения для Enother_branch ветки<br>');
-print ('Здесь у нас будет код, который потом появится в коммите<br>');
+print ('<ul>
+  <li>Почистить посудомоечную машину</li>
+  <li><del datetime="2009-10-11T01:25-07:00">Погулять</del></li>
+  <li><del datetime="2009-10-10T23:38-07:00">Поспать</del></li>
+  <li><ins>Купить принтер</ins></li>
+</ul><br>');
+print ('<article>
+  <div class="highlight">
+    <p>Кryptologin cмерджил kryptologins_branch</p>
+    <p>Новый коммит от kryptologin</p>
+  </div>
+  <p>Текст, в котором <span>выделена фраза</span></p>
+</article>');
 print ('Кryptologin cмерджил kryptologins_branch<br>');
 print ('новая строка в которой будут изминения для Enother_branch ветки-2<br>');
 print ('Кryptologin cмерджил kryptologins_branch<br>');
 print ('новый коммит от kryptologins_branch<br>');
 print ('Ссылка на <a href="/DelitedFile.php">DelitedFile</a> <br>');
 print ('Ссылка на <a href="/hello_GIT.php">hello_GIT</a> <br>');
+print ('Ссылка на <a href="/newList.php">newList_GIT</a> <br>');
--- newList.php
+++ newList.php
@@ -1,7 +1,7 @@
 <?php
 // init this file
-print ('<h1>DelitedFile</h1>');
-print("Пока это копиия с DelitedFile.php, но вскоре мы превратим его в полноценную страницу<br>");
+    print ('<h1>newList</h1>');
+print("добавляем текст с iMac'27<br>");
 print ('еще одна строка для обновления файла в GIT<br>');
 print ('какой-то еще код<br>');
 print ('какой-то еще код2<br>');
